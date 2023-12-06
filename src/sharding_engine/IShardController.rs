9f747a27bb6eb54027efc2674926abc873e783f6
estemzephyr <yusufayd2307@gmail.com>
2023-12-06 10:03:34 +0300

    Sharding engine is building.

--- src/sharding_engine/IShardController.rs
+++ src/sharding_engine/IShardController.rs
@@ -1,35 +1,78 @@
+use std::cmp::Ordering;
 use crate::IDataObj::IData::IData;
-use crate::sharding_engine::Ishard::IShard;
-const ALPHABET:&str = "abcdefghijklmnopqrstuvwxyz";
+use crate::sharding_engine::Ishard::{IShard, Shards};
+
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+
 pub enum ControlProtocol {
     Default,
     Alphabetic,
     MostView,
     Shuffled,
 }
+// A function to take first char of data
+pub fn take_first_char(data: &Vec<IData>) -> char {
+    data.iter().flat_map(|shard| shard.value.chars().next()).next().unwrap_or('x')
+}
+
 impl ControlProtocol {
     pub fn default() -> ControlProtocol {
         ControlProtocol::Default
     }
 
-    pub fn list_shard_with_algorithm(self, mut data: IShard){
+    pub async fn list_shard_with_algorithm_and_insert(self, mut shards: Vec<IShard>) -> Shards {
+        let mut shard = IShard::default();
+
         match self {
             ControlProtocol::Alphabetic => {
-                println!("{:?}",data.ivalue);
 
+                //TODO
+                // Call the threads for data processing in lifetime to map
+                // Overload the memory to take data lifetimes.
+                // Process the data's and list with algorithm and send ShardService
+
+
+
+                // Memory Loading with threads
+
+
+                let s = Shards::default();
+                Shards::new_shard_vec(s).await
             }
-            ControlProtocol::Default => {
-                // Handle the default case here if needed
-                unimplemented!()
-            }
-            ControlProtocol::MostView => {
-                // Handle MostView case
-                unimplemented!()
-            }
-            ControlProtocol::Shuffled => {
-                // Handle Shuffled case
-                unimplemented!()
+
+                ControlProtocol::Default => {
+                    // Handle the default case here if needed
+                    let s = Shards::default();
+                    Shards::new_shard_vec(s).await
+                }
+                ControlProtocol::MostView => {
+                    // Handle MostView case
+                    let s = Shards::default();
+                    Shards::new_shard_vec(s).await
+                }
+                ControlProtocol::Shuffled => {
+                    // Handle Shuffled case
+                    let s = Shards::default();
+                    Shards::new_shard_vec(s).await
+                }
             }
         }
+    }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_algorithm() {
+        let data = IData {
+            id: 0,
+            name: "Car".to_string(),
+            value: "Mustang".to_string(),
+        };
+        //let first_char = take_first_char();
+        //println!("{:?}",first_char)
+        //let _shards = ControlProtocol::list_shard_with_algorithm_and_insert(ControlProtocol::Alphabetic, data).await;
+        //println!("{:?}",_shards)
     }
 }
--- src/sharding_engine/Ishard.rs
+++ src/sharding_engine/Ishard.rs
@@ -6,48 +6,57 @@ lazy_static! {
     static ref ID_COUNTER: Mutex<i32> = Mutex::new(0);
 }
 //Delete After Test
-#[derive(Clone,Debug)]
-pub struct IShard{
-    pub(crate) key:String,
-    pub(crate) id:i32,
-    pub(crate) ivalue:Vec<IData>,
+#[derive(Clone, Debug)]
+pub struct IShard {
+    pub(crate) key: String,
+    pub(crate) id: i32,
+    pub(crate) ivalue: Vec<IData>,
 }
+
+#[derive(Debug)]
 pub struct Shards {
     id: u32,
     key: String,
     shards: Vec<IShard>,
 }
-pub async fn sharding_process(shard:IShard) {
-    let mut shard_vec = Shards::new_shard_vec().await;
-    let mut block_counter = 0;
-    shard_vec.shards.push(shard);
-    for id in 0..=shard_vec.id {
-        if id >= 50 {
-            shard_vec.id = 0;
-            shard_vec.key = format!("Block Number :{}", &block_counter);
-            block_counter += 1;
-        } else {
-            continue;
-        }
-    }
-}
+
 
 impl Shards {
-    pub async fn new_shard_vec() -> Shards {
+    //Constructor block
+    pub(crate) fn default() -> Shards{
+        Shards{
+            id: 0,
+            key: "".to_string(),
+            shards: vec![],
+        }
+    }
+    pub(crate) async fn new_shard_vec(self) -> Shards {
         let mut id_counter = 0;
         let default_shard = Shards {
             id: id_counter,
             key: String::new(),
-            shards: vec![],
+            shards: self.shards,
         };
         id_counter += 1;
         default_shard
+    }
+    pub async fn sharding_process(_shard: Vec<IShard>, key: char) {
+        //Shadowing for security:Value mutable for working lifetime , after process value isn't mutable.
+
+        //Note: 'key' define in IShardController.rs
+        let mut _shard = _shard;
+        for mut value in _shard {
+            if value.id >= 100{
+                value.id = 0;
+            }else { continue }
+            _shard.push(value)
+        }
     }
 }
 
 impl IShard {
     pub fn default() -> IShard {
-        IShard{
+        IShard {
             key: String::new(),
             id: 0,
             ivalue: vec![],
@@ -57,6 +66,6 @@ impl IShard {
         let mut counter = ID_COUNTER.lock().unwrap();
         self.id = *counter;
         *counter += 1;
-        IShard {key: self.key, id:self.id, ivalue: Vec::new() }
+        IShard { key: self.key, id: self.id, ivalue: Vec::new() }
     }
 }
